197bbcc610e4ec03da130538dbe119fcafa1f650
ayanchaki61 <88726187+ayanchaki61@users.noreply.github.com>
2026-01-18 23:02:09 +0530

    Update CORS to allow Vercel frontend domain

--- server/server.js
+++ server/server.js
@@ -12,7 +12,13 @@ const authRoutes = require('./routes/auth');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://musicworldroan.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
